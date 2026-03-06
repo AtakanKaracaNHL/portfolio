f65db83c2c4e81aaa374a327f646830d75f34e8f
Ubuntu <ubuntu@vps-a9148f71.vps.ovh.net>
2026-03-06 13:12:49 +0000

    Permission update

--- whoami.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-require __DIR__ . '/includes/config.php';
-require __DIR__ . '/includes/helpers.php';
-require __DIR__ . '/includes/auth.php';
-
-echo '<pre>';
-var_dump(current_user());
